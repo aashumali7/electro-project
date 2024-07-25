cdab454eb46c47a8fcd906d4870bf9ecd3f8537e
aashumali <aashishmali223@gmail.com>
2024-07-25 12:30:21 +0530

    search done

--- app/Http/Controllers/ProductFilterController.php
+++ app/Http/Controllers/ProductFilterController.php
@@ -13,11 +13,25 @@ class ProductFilterController extends Controller
 
     //3. Method
    
-    public function filter(){
-        $products = DB::table('products')
-        ->join('brands', 'products.brand_id', '=', 'brands.id')
-        ->join('categories', 'products.category_id', '=', 'categories.category_id')
-        ->get();
+    public function filter(Request $request){
+        //dd($request->all());
+        //check if query parameters is set in url 
+        if($request->all()){
+            $q = $request->q;
+            //dd($q);
+            $products = DB::table('products')
+            ->join('brands', 'products.brand_id', '=', 'brands.id')
+            ->join('categories', 'products.category_id', '=', 'categories.category_id')
+            ->where('products.product_name', 'like', '%' . $q . '%')
+            ->get();
+
+        }else{
+            $products = DB::table('products')
+            ->join('brands', 'products.brand_id', '=', 'brands.id')
+            ->join('categories', 'products.category_id', '=', 'categories.category_id')
+            ->get();
+        }
+       
 
         $brands = DB::table('products')
         ->select('brands.brand_name', DB::raw('COUNT(*) as productCount'))
